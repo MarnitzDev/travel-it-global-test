22bded064366ddd03e9101c3e85b68dae216ce28
Marnitz Malan <malanmarnitz@gmail.com>
2025-10-16 10:37:14 +0200

    feat: fetchRepos

--- src/views/RepoView.vue
+++ src/views/RepoView.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
+import { useGithubStore } from '../stores/github';
 
 const route = useRoute();
+const store = useGithubStore();
 
 const username = computed(() => route.params.username as string);
 
+onMounted(async () => {
+  await store.fetchRepos(username.value);
+});
 </script>
 
 <template>
@@ -14,6 +19,12 @@ const username = computed(() => route.params.username as string);
         
         <section class="repos">
             <h2>Repositories</h2>
+            <ul v-if="store.repos.length">
+                <li v-for="repo in store.repos" :key="repo.name">
+                    <p>{{ repo.name }}</p>
+                </li>
+            </ul>
+            <p v-else>No repositories found.</p>
         </section>
     </div>
 </template>
